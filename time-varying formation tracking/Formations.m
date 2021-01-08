a653eb716d7048783c06d88313bafbabae98394a
zeyang23 <zeyang23@outlook.com>
2021-01-08 14:05:13 +0800

    Formations class explanation

--- time-varying formation tracking/Formations.m
+++ time-varying formation tracking/Formations.m
@@ -1,36 +1,40 @@
-% Class: Formations. for dynamic formation selection
+% Class for dynamic formation selection
 classdef Formations < handle
     properties
-        robot_number;
-        Targets;% desired points(3-D array)
-        Incidences;% cell format 
-        Laplacians;% 3-D array
-        Connects;
-        current_index;% Current formation tracked by robots
-        formation_number;
+        robot_number; % the number of robots
+        Targets;      % desired target position for all robots (3-D array)
+        Incidences;   % incidence matrices for all graphs (cell) 
+        Laplacians;   % Laplacians for all graphs (3_D array) 
+        Connects;     % define all the edges of all graphs (cell)
+        current_index;% the index of current target formation
+        formation_number; % the number of formations
         
-        global_error;
-        local_error;
+        global_error; % current global error for all formations (1-d array)
+        local_error;  % current local error for all formations (2-d array)
         
         % formation error log
         % every row is the global error trajectory of one formation
-        global_error_log
+        
+        global_error_log % record the history of global error (2-d array)
         
         % define as cell
         % each matrix in the cell is the local error trajectory of one formation
-        local_error_log           
-        global_error_estimate_log 
-        estimator_state_log
+        
+        local_error_log % record the history of local error (cell)         
+        global_error_estimate_log % record the history of global error estimate (cell)
+        estimator_state_log % record the history of estimator state (cell)
         
         % parameters for dynamic formation selection
-        decision_counter; % used to config when to make decisions
-        global_error_estimate; 
-        estimator_state;
-        info_rate;
+        decision_counter; % increase by 1 at every time step
+        global_error_estimate; % current global error estimate for all formations (2-d array)
+        estimator_state;       % current estimator state for all formations (2-d array)
+        info_rate;             % the weight of instantaneous information in proportional-integral estimator
     end
     
     methods
         function item = Formations(targets,connects)
+        % calculate necessary properties at initialization
+            
             item.robot_number=size(targets,1);
             item.Targets=targets;
             item.Connects=connects;
@@ -39,8 +43,11 @@ classdef Formations < handle
             item.decision_counter=1;
      
         end
-               
+        
+        
         function cal_matrices(obj)
+        % calculate all the matrices of all formations
+        
               n = obj.robot_number;% n is the robot number
               obj.Laplacians =zeros(n,n,obj.formation_number);
               obj.Incidences = cell(1,obj.formation_number);
@@ -60,6 +67,8 @@ classdef Formations < handle
         
         
         function cal_global_error(obj,ksi)
+        % calculate current global error for all formations
+        
             obj.global_error=zeros(obj.formation_number,1);
             pos = get_pos(ksi);
             for i =1 : obj.formation_number
@@ -75,6 +84,8 @@ classdef Formations < handle
         
         
         function cal_local_error(obj,ksi)
+        % calculate current local error of for all formations
+        
             obj.local_error=zeros(obj.robot_number,obj.formation_number);
              pos = get_pos(ksi);
             for k =1:obj.formation_number
@@ -91,7 +102,10 @@ classdef Formations < handle
             
         end
         
+        
         function make_decision(obj)
+        % robots make their decisions to choose their target formation
+        
             robot_decisions=zeros(obj.robot_number,1);
             for i=1:obj.robot_number
                 [~,index]=min(obj.global_error_estimate(i,:));
@@ -109,7 +123,9 @@ classdef Formations < handle
             end
         end
         
+        
         function update_estimate(obj,ksi,gamma,h,Tconv)
+        % update the important functions in dynamic formation selection algotithm
             
             if (obj.decision_counter == 1)
                 obj.init_estimate(ksi,gamma);
@@ -155,7 +171,10 @@ classdef Formations < handle
 
         end
         
+        
         function init_estimate(obj,ksi,gamma)
+        % initialize the values of functions in dynamic formation selection algotithm
+        
             obj.info_rate=gamma;
             obj.cal_global_error(ksi);
             obj.cal_local_error(ksi);
@@ -166,17 +185,26 @@ classdef Formations < handle
                      
         end
         
+        
         function update_counter(obj)
+        % update decision_counter
+        
             obj.decision_counter=obj.decision_counter+1;
         end
         
+        
         function centralized_decision(obj,ksi)
+        % centralized dynamic formation selection
+        
             obj.cal_global_error(ksi);
             [~,index]=min(obj.global_error);
             obj.current_index=index;
         end
         
+        
         function initialize_log(obj)
+        % initialize logs of functions in dynamic formation selection
+        
             % initialize log
             obj.global_error_log=obj.global_error;
             obj.local_error_log = cell(1,obj.formation_number);
@@ -190,7 +218,10 @@ classdef Formations < handle
             end
         end
         
+        
         function update_log(obj)
+        % update logs of functions in dynamic formation selection
+        
             obj.global_error_log=[obj.global_error_log,obj.global_error];
             for m =1:obj.formation_number
                 obj.local_error_log{m}=[obj.local_error_log{m},obj.local_error(:,m)];
